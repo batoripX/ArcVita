c9894a027849cdc7796ec13dad20e34cd7e8fb40
coderX <bbatoripers@gmail.com>
2025-11-09 17:57:55 +0400

    files

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,9 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_arcvitadev_arcvita_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
+    // TODO: implement stringFromJNI()
+}
